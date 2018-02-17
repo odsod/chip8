b9e02c0def477f8a90da49cc04c2cfebef87ae5f
Oscar David Söderlund <odsod@users.noreply.github.com>
2018-02-17 18:09:07 +0100

    Render frame and title

--- cmd/chip8/main.go
+++ cmd/chip8/main.go
@@ -50,8 +50,33 @@ func (r Random) Next() uint8 {
 	return uint8(rand.Uint32())
 }
 
-func renderToTermbox(vm *chip8.VM) {
+func renderTitle(x0, y0 int, s string) {
+	for xi, c := range s {
+		termbox.SetCell(x0+xi, y0, c, termbox.ColorWhite, termbox.ColorDefault)
+	}
+}
+
+func renderBorder(x0, y0, w, h int, borderColor termbox.Attribute) {
+	x1 := x0 + w
+	y1 := y0 + h
+	termbox.SetCell(x0, y0, '╔', borderColor, termbox.ColorDefault)
+	termbox.SetCell(x0, y1, '╚', borderColor, termbox.ColorDefault)
+	termbox.SetCell(x1, y0, '╗', borderColor, termbox.ColorDefault)
+	termbox.SetCell(x1, y1, '╝', borderColor, termbox.ColorDefault)
+	for x := x0 + 1; x < x1; x++ {
+		termbox.SetCell(x, y0, '═', borderColor, termbox.ColorDefault)
+		termbox.SetCell(x, y1, '═', borderColor, termbox.ColorDefault)
+	}
+	for y := y0 + 1; y < y1; y++ {
+		termbox.SetCell(x0, y, '║', borderColor, termbox.ColorDefault)
+		termbox.SetCell(x1, y, '║', borderColor, termbox.ColorDefault)
+	}
+}
+
+func render(romName string, vm *chip8.VM) {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	renderTitle(0, 0, "CHIP-8: "+romName)
+	renderBorder(0, 1, 65, 33, termbox.ColorWhite)
 	for y, scanLine := range vm.VideoMemory {
 		for x := 0; x < 64; x++ {
 			pixel := scanLine&(0x8000000000000000>>uint(x)) > 0
@@ -61,7 +86,7 @@ func renderToTermbox(vm *chip8.VM) {
 			} else {
 				bg = termbox.ColorDefault
 			}
-			termbox.SetCell(x, y, ' ', termbox.ColorDefault, bg)
+			termbox.SetCell(1+x, 2+y, ' ', termbox.ColorDefault, bg)
 		}
 	}
 	termbox.Flush()
@@ -150,7 +175,7 @@ Loop:
 		case <-timerTicks.C:
 			vm.TickTimers()
 		case <-videoRefreshes.C:
-			renderToTermbox(vm)
+			render(*romFile, vm)
 		case <-killChannel:
 			break Loop
 		}
